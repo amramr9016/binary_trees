a86040c82803d8298416e6e377db56469465eb62
3mr <amramr9016@gmail.com>
2024-03-27 08:03:08 -0700

    kkk

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes a binary tree.
+ * @root: A pointer to the root node of the tree to delete.
+ */
+void binary_tree_delete(binary_tree_t *root)
+{
+	if (root != NULL)
+	{
+	binary_tree_delete(root->left);
+	binary_tree_delete(root->right);
+	free(root);
+	}
+}
