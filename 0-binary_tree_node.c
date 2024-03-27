1065a77788b33afa2031c48edd42ac826e41d205
3mr <amramr9016@gmail.com>
2024-03-27 07:59:11 -0700

    Done

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,20 +6,20 @@
  * @val: The value to put in the new node.
  *
  * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
+ *	Otherwise - a pointer to the new node.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *mother, int val)
 {
-    binary_tree_t *brand_new;
+	binary_tree_t *brand_new;
 
-    brand_new = malloc(sizeof(binary_tree_t));
-    if (brand_new == NULL)
-        return (NULL);
+	brand_new = malloc(sizeof(binary_tree_t));
+	if (brand_new == NULL)
+	return (NULL);
 
-    brand_new->n = val;
-    brand_new->parent = mother;
-    brand_new->left = NULL;
-    brand_new->right = NULL;
+	brand_new->n = val;
+	brand_new->parent = mother;
+	brand_new->left = NULL;
+	brand_new->right = NULL;
 
-    return (brand_new);
+	return (brand_new);
 }
